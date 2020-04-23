4cf067f79ef4022fab4140b2051a4af088763510
JisooJang <ppuagirls@naver.com>
2020-04-24 00:05:23 +0900

    feat: add Calendar bean in config class.

--- src/main/java/com/example/mycoupon/config/AppConfig.java
+++ src/main/java/com/example/mycoupon/config/AppConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+import java.util.Calendar;
+
 @Configuration
 public class AppConfig {
+    @Bean
+    public Calendar calendar() {
+        return Calendar.getInstance();
+    }
 }
