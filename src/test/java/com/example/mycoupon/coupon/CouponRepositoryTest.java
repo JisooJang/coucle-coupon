20db2c28941ba16ab2a26615ce6cfccd9970a8f8
JisooJang <ppuagirls@naver.com>
2020-04-24 22:14:17 +0900

    feat: implements CouponRepositoryTest codes

--- src/test/java/com/example/mycoupon/coupon/CouponRepositoryTest.java
+++ src/test/java/com/example/mycoupon/coupon/CouponRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.mycoupon.coupon;
 import com.example.mycoupon.domain.coupon.Coupon;
 import com.example.mycoupon.domain.coupon.CouponRepository;
 import com.example.mycoupon.domain.couponInfo.CouponInfo;
+import com.example.mycoupon.domain.member.Member;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +12,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,29 +39,62 @@ public class CouponRepositoryTest {
 
             CouponInfo couponInfo = CouponInfo.builder()
                     .coupon(coupon)
-                    .isEnabled(true)
+                    .isUsed(false)
                     .build();
             couponInfo = this.entityManager.persist(couponInfo);
         }
+
+        Member member = Member.builder().mediaId("test1").password("qwerQQ1234!!").build();
+        member = this.entityManager.persist(member);
+
+        Coupon coupon1 = this.entityManager.find(Coupon.class, 1L);
+        Coupon coupon2 = this.entityManager.find(Coupon.class, 2L);
+
+        Date nowDate = new Date();
+        coupon1.setMember(member);
+        coupon1.setAssignedAt(nowDate);
+        coupon1.setExpiredAt(nowDate);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(nowDate);
+        cal.add(Calendar.DATE, 2);
+
+        coupon2.setMember(member);
+        coupon2.setAssignedAt(nowDate);
+        coupon2.setExpiredAt(cal.getTime());
+
+        this.entityManager.flush();  // UPDATE DB 반영
     }
     @Test
     public void findByFreeUsers() throws Exception {
         Coupon freeCoupon = couponRepository.findByFreeUser();
-        assertThat(freeCoupon.getCode()).isEqualTo("code1");
+        assertThat(freeCoupon).isNotNull();
+        assertThat(freeCoupon.getMember()).isNull();
+        assertThat(freeCoupon.getAssignedAt()).isNull();
+        assertThat(freeCoupon.getExpiredAt()).isNull();
     }
 
     @Test
     public void findByCode() throws Exception {
-
+        String findCode = "code1";
+        Coupon coupon = couponRepository.findByCode(findCode);
+        assertThat(coupon).isNotNull();
+        assertThat(coupon.getCode()).isEqualTo(findCode);
     }
 
     @Test
     public void findByMemberId() throws Exception {
-
+        List<Coupon> results = couponRepository.findByMemberId(1);
+        assertThat(results).size().isEqualTo(2);
+        assertThat(results.get(0).getMember().getId()).isEqualTo(1);
+        assertThat(results.get(1).getMember().getId()).isEqualTo(1);
     }
 
     @Test
     public void findByExpiredToday() throws Exception {
-
+        List<Coupon> results = couponRepository.findByExpiredToday();
+        assertThat(results).size().isEqualTo(1);
+        assertThat(results.get(0).getMember()).isNotNull();
+        assertThat(results.get(0).getAssignedAt()).isNotNull();
     }
 }
