141866970ecc91bbc7bfdbf7bc9c7a057d1da5ef
JisooJang <ppuagirls@naver.com>
2020-04-25 17:16:51 +0900

    fix: fix password regex

--- src/main/java/com/example/mycoupon/utils/ValidationRegex.java
+++ src/main/java/com/example/mycoupon/utils/ValidationRegex.java
@@ -1,8 +1,8 @@
-package com.example.mycoupon.common;
+package com.example.mycoupon.utils;
 
 public class ValidationRegex {
     // 숫자, 문자, 특수문자 각각 1개 이상 포함하여 최소 8자리 이상
-    public static final String PASSWORD = "^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+    public static final String PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
     // UUID format
     public static final String COUPONCODE = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$";
 }
