f786d128bdd3c76ef6db27b4f8f918c1ea63350b
JisooJang <ppuagirls@naver.com>
2020-04-25 19:20:27 +0900

    fix: success MemberRepositoryTest

--- src/test/java/com/example/mycoupon/member/MemberRepositoryTest.java
+++ src/test/java/com/example/mycoupon/member/MemberRepositoryTest.java
@@ -2,16 +2,17 @@ package com.example.mycoupon.member;
 
 import com.example.mycoupon.domain.member.Member;
 import com.example.mycoupon.domain.member.MemberRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -72,5 +73,38 @@ public class MemberRepositoryTest {
 
     }
 
-    // TODO: add invalid save() (raise exception)
+    @Test(expected = ConstraintViolationException.class)
+    public void savePasswordValidationFailed() {
+        String testMediaId = "test1234";
+        Member m = Member.builder()
+                .mediaId(testMediaId)
+                .password(passwordEncoder.encode("qwer1234"))
+                .build();
+
+        Member result = memberRepository.save(m);
+    }
+
+    @Test(expected = ConstraintViolationException.class)
+    public void saveIdValidationFailed() {
+        String testMediaId = " ";
+        Member m = Member.builder()
+                .mediaId(testMediaId)
+                .password(passwordEncoder.encode("qwer1234!"))
+                .build();
+
+        Member result = memberRepository.save(m);
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void saveMemberIdAlreadyExistsFailed() {
+        for(int i=0 ; i<2 ; i++) {
+            String testMediaId = "test1234";
+            Member m = Member.builder()
+                    .mediaId(testMediaId)
+                    .password(passwordEncoder.encode("qwer1234!"))
+                    .build();
+
+            Member result = memberRepository.save(m);
+        }
+    }
 }
