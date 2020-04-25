5ed7f946b9fe15fc6ab2f0fde485ebcfe6f585ca
JisooJang <ppuagirls@naver.com>
2020-04-26 01:49:33 +0900

    fix: fix error_message format in CustomControllerAdvice

--- src/main/java/com/example/mycoupon/controller/CustomControllerAdvice.java
+++ src/main/java/com/example/mycoupon/controller/CustomControllerAdvice.java
@@ -1,7 +1,8 @@
-package com.example.mycoupon.common;
+package com.example.mycoupon.controller;
 
 import com.example.mycoupon.exceptions.IllegalArgumentException;
 import com.example.mycoupon.exceptions.CouponNotFoundException;
+import com.example.mycoupon.exceptions.MemberNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,9 +18,9 @@ public class CustomControllerAdvice {
     private final MediaType vndErrorMediaType =
             MediaType.parseMediaType("application/vnd.error");
 
-    @ExceptionHandler(CouponNotFoundException.class)
-    ResponseEntity<Object> notFoundException(CouponNotFoundException e) {
-        return this.error(e, HttpStatus.NOT_FOUND, e.getCouponCode());
+    @ExceptionHandler(value = {CouponNotFoundException.class, MemberNotFoundException.class})
+    ResponseEntity<Object> notFoundException(Exception e) {
+        return this.error(e, HttpStatus.NOT_FOUND, e.getLocalizedMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -27,13 +28,10 @@ public class CustomControllerAdvice {
         return this.error(e, HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
     }
 
-    private <E extends Exception> ResponseEntity<Object> error(E error, HttpStatus httpStatus, String logref) {
-        String msg = Optional.of(error.getMessage())
-                .orElse(error.getClass().getSimpleName());
-
+    private <E extends Exception> ResponseEntity<Object> error(E error, HttpStatus httpStatus, String msg) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(this.vndErrorMediaType);
-        return new ResponseEntity<>(logref + ":" + msg,
+        return new ResponseEntity<>(msg,
                 httpHeaders, httpStatus);
     }
 
