831bdfdb5ee7b927dba5d3ed273f0d4246c36eb4
JisooJang <ppuagirls@naver.com>
2020-04-26 18:00:51 +0900

    fix: add comments in JwtAuthorizationFilter

--- src/main/java/com/example/mycoupon/config/security/filters/JwtAuthorizationFilter.java
+++ src/main/java/com/example/mycoupon/config/security/filters/JwtAuthorizationFilter.java
@@ -2,10 +2,15 @@ package com.example.mycoupon.config.security.filters;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.InvalidClaimException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.example.mycoupon.config.security.JWTSecurityConstants;
+import com.example.mycoupon.exceptions.InvalidTokenException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -43,15 +48,23 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
         chain.doFilter(req, res);
     }
 
-    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) throws IOException {
         // get JWT from headers and validate token.
         String token = request.getHeader(JWTSecurityConstants.HEADER_STRING);
         if(token != null) {
-                String user = JWT.require(Algorithm.HMAC512(JWTSecurityConstants.SECRET.getBytes()))
+            String user = null;
+            try {
+                user = JWT.require(Algorithm.HMAC512(JWTSecurityConstants.SECRET.getBytes()))
                         .build()
                         .verify(token.replace(JWTSecurityConstants.TOKEN_PREFIX, ""))
                         //.getSubject();
                         .getAudience().get(0);
+            } catch(JWTDecodeException | InvalidClaimException e) {
+//                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//                PrintWriter writer = response.getWriter();
+//                writer.println("Invalid Authorization Token.");
+                throw new InvalidTokenException(e, HttpStatus.UNAUTHORIZED.value());
+            }
 
             if(user != null) {
                 // 인증이 완료된 토큰 return
@@ -61,4 +74,12 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
         }
         return null;
     }
+
+//    @Override
+//    protected void onUnsuccessfulAuthentication(HttpServletRequest request,
+//                                                HttpServletResponse response, AuthenticationException failed) throws IOException {
+//
+//        System.out.println(request);
+//        System.out.println(response);
+//    }
 }
