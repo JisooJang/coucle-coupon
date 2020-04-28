4c91de196f51b915fb0d74c569423d179d8e9401
JisooJang <ppuagirls@naver.com>
2020-04-29 00:13:17 +0900

    fix: delete comments

--- src/main/java/com/example/mycoupon/repository/CouponRepository.java
+++ src/main/java/com/example/mycoupon/repository/CouponRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    // TODO: 1개의 결과값만 받도록 쿼리
     @Query(value = "SELECT * from coupon WHERE member_id IS NULL LIMIT 1", nativeQuery = true)
-    //@Query(value = "UPDATE coupon SET user_id = ?1 WHERE id = (SELECT id from coupon WHERE user_id IS NULL LIMIT 1)", nativeQuery = true)
     public Coupon findByFreeUser();
 
     @Query(value = "SELECT * from coupon WHERE FORMATDATETIME(EXPIRED_AT, 'yyyy-MM-dd') = CURRENT_DATE()", nativeQuery = true)
