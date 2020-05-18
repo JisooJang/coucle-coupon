fc624438da2b0540c20a5a0914dec02a1e71fed6
JisooJang <ppuagirls@naver.com>
2020-05-19 01:16:05 +0900

    feat: add AOP aspect and annotaion interface in aop package

---
+++ src/main/java/com/example/mycoupon/aop/AopAspect.java
@@ -0,0 +1,25 @@
+package com.example.mycoupon.aop;
+
+import com.example.mycoupon.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class AopAspect {
+    // define pointcut and advice
+    @Around(value="@annotation(LogExecutionTime) && args(member)", argNames = "member")
+    public Object LogExecutionTime(ProceedingJoinPoint joinPoint, Member member) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed(); // method return value
+        long executionTime = System.currentTimeMillis() - startTime;
+
+        log.info(joinPoint.getSignature() + " executed in " + executionTime);
+        log.info("ID: " + member.getMediaId() + " assigned to coupon(" + proceed + ")");
+        return proceed;
+    }
+}
