408be737a0952582129afc6617a1f52ee109e4fc
JisooJang <ppuagirls@naver.com>
2020-05-26 17:05:25 +0900

    feat: add CouponSchedulerServiceTest class

---
+++ src/test/java/com/example/mycoupon/service/CouponSchedulerServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.mycoupon.service;
+
+import com.example.mycoupon.domain.Coupon;
+import com.example.mycoupon.domain.Member;
+import com.example.mycoupon.repository.CouponInfoRepository;
+import com.example.mycoupon.repository.CouponRepository;
+import org.hibernate.validator.constraints.ModCheck;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CouponSchedulerServiceTest {
+    @Mock
+    private CouponRepository couponRepository;
+
+    private CouponSchedulerService schedulerService;
+
+    @Before
+    public void prepare() {
+        this.schedulerService = new CouponSchedulerService(couponRepository);
+    }
+    @Test
+    public void sendNoticeExpiredAfter3days() {
+        LocalDateTime nowDateLocal = LocalDateTime.now();
+        List<Coupon> couponList = new ArrayList<>();
+
+        for(int i=0 ; i < 100 ; i++) {
+            Member m = Member.builder()
+                    .mediaId("test" + i)
+                    .password("test1234!")
+                    .build();
+
+            Coupon coupon = Coupon.builder()
+                    .code(UUID.randomUUID().toString())
+                    .assignedAt(nowDateLocal)
+                    .expiredAt(nowDateLocal.plusDays((long)3))
+                    .member(m)
+                    .build();
+            couponList.add(coupon);
+        }
+
+        // mock return data
+        given(couponRepository.findByExpiredAfter3Days()).willReturn(couponList);
+
+        schedulerService.sendNoticeExpiredAfter3days();
+    }
+}
