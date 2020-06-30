6278d8c7a967d71181a823de024c22fb2db82d90
JisooJang <ppuagirls@naver.com>
2020-07-01 00:56:48 +0900

    fix: add setting for handling LocalDateTime serialize error issue. https://stackoverflow.com/questions/45863678/json-parse-error-can-not-construct-instance-of-java-time-localdate-no-string-a?rq=1

--- src/main/java/com/example/mycoupon/MycouponApplication.java
+++ src/main/java/com/example/mycoupon/MycouponApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @SpringBootApplication
 public class MycouponApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MycouponApplication.class, args);
     }
