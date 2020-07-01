9e747b6278461f4394b1f1d2f7b88f8c8e75b1e8
JisooJang <ppuagirls@naver.com>
2020-07-01 16:30:20 +0900

    feat: add spring cloud feign dependency

--- src/main/java/com/example/mycoupon/service/CouponService.java
+++ src/main/java/com/example/mycoupon/service/CouponService.java
@@ -123,9 +123,9 @@ public class CouponService {
         return coupon.getCode();
     }
 
-//    public String testTransactionalProxy(Member m) throws InterruptedException {
-//        return assignToUser(m);
-//    }
+    public String testTransactionalProxy(Member m) throws InterruptedException {
+        return assignToUser(m);
+    }
 
     @Transactional
     public void updateIsEnabledCouponById(String code, long memberId, boolean isUsed) throws CouponNotFoundException {
