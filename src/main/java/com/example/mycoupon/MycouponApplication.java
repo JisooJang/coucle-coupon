eeb9ac774570e1fa7d0adb0f9734ac4ab816012e
JisooJang <ppuagirls@naver.com>
2020-07-01 14:52:56 +0900

    feat: add spring cloud netflix eureka dependency, and add bootstrap.properties to configure eureka settings.

--- src/main/java/com/example/mycoupon/MycouponApplication.java
+++ src/main/java/com/example/mycoupon/MycouponApplication.java
@@ -3,8 +3,10 @@ package com.example.mycoupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableDiscoveryClient
 @EnableCaching
 @EnableAsync
 @SpringBootApplication
