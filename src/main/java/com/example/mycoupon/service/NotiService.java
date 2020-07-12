29271485f99dd9b8be4b9f59605950bf2bd2c9ba
JisooJang <ppuagirls@naver.com>
2020-07-12 23:53:07 +0900

    refactor: implement sendNoticeToUser method using kafkaTemplate in NotiService class

--- src/main/java/com/example/mycoupon/service/NotiService.java
+++ src/main/java/com/example/mycoupon/service/NotiService.java
@@ -1,15 +1,27 @@
 package com.example.mycoupon.service;
 
+import com.example.mycoupon.template.AlarmTalk;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class NotiService {
+    private final KafkaTemplate<String, AlarmTalk> kafkaTemplate;
+
+    @Autowired
+    public NotiService(KafkaTemplate<String, AlarmTalk> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Async
-    public void sendNoticeToUser(String userId) {
+    public void sendAlarmTalkToUser(AlarmTalk alarmTalk) {
         // 테스트용으로 로그만 찍음. 향후 알림톡을 보내거나 메시지 큐 구조로 메시지를 쏘는 등의 작업이 될 수 있다.
-        log.info("Your coupon expires in 3 days. userId : " + userId + " - " + Thread.currentThread().getName());
+        log.info("Your coupon expires in 3 days. userId : " + alarmTalk.getMediaId() + " - " + Thread.currentThread().getName());
+        kafkaTemplate.send("alarmtalk.notification", alarmTalk);
+
     }
 }
