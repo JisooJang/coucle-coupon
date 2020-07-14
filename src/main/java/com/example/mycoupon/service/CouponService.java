68b41f6bd13781664b1db3cdfa02aa80f2e84494
JisooJang <ppuagirls@naver.com>
2020-07-15 01:32:59 +0900

    refactor: refactor couponService (CompletableFuture, method 분리...)

--- src/main/java/com/example/mycoupon/service/CouponService.java
+++ src/main/java/com/example/mycoupon/service/CouponService.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -57,25 +58,37 @@ public class CouponService {
     // 트랜잭션 전파 유형 : PROPAGATION_REQUIRED (기본값)
     // 이미 존재하는 부모 트랜잭션이 있다면 부모 트랜잭션 내에서 실행되고, 부모 트랜잭션이 없다면 새 트랜잭션이 시작된다.
     @Transactional
-    public Coupon save(Member member) {
-        /* member 매개변수는 null로 넘어올 수 있다. (유저에게 할당하기 전에 쿠폰을 생성할 때) */
-        Coupon coupon;
+    public Coupon save() {
         LocalDateTime nowDateLocal = LocalDateTime.now();
-        if(member == null) {
-            coupon = Coupon.builder()
-                    .code(getUUIDCouponCode())
-                    .build();
-        } else {
-            coupon = Coupon.builder()
-                    .code(getUUIDCouponCode())
-                    .assignedAt(nowDateLocal)
-                    .expiredAt(getRandomExpiredAt(nowDateLocal))
-                    .member(member)
-                    .build();
-        }
+        Coupon coupon = Coupon.builder()
+                .code(getUUIDCouponCode())
+                .build();
 
         Coupon couponResult = couponRepository.save(coupon);
-        CouponInfo couponInfo = CouponInfo.builder().couponId(couponResult.getId()).isUsed(false).build();
+        CouponInfo couponInfo = CouponInfo.builder()
+                .couponId(couponResult.getId())
+                .isUsed(false)
+                .build();
+
+        couponInfoRepository.save(couponInfo);
+        return couponResult;
+    }
+
+    @Transactional
+    public Coupon saveByMember(Member member) {
+        LocalDateTime nowDateLocal = LocalDateTime.now();
+        Coupon coupon = Coupon.builder()
+                .code(getUUIDCouponCode())
+                .assignedAt(nowDateLocal)
+                .expiredAt(getRandomExpiredAt(nowDateLocal))
+                .member(member)
+                .build();
+
+        Coupon couponResult = couponRepository.save(coupon);
+        CouponInfo couponInfo = CouponInfo.builder()
+                .couponId(couponResult.getId())
+                .isUsed(false)
+                .build();
 
         couponInfoRepository.save(couponInfo);
         return couponResult;
@@ -84,43 +97,45 @@ public class CouponService {
     @CacheEvict(value="coupon-list", key="#member.id")
     @LogExecutionTime
     @Transactional
-    public String assignToUserAsync(Member member) throws ExecutionException, InterruptedException {
+    public CompletableFuture<String> assignToUserAsync(Member member) throws ExecutionException, InterruptedException {
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            Coupon coupon = couponRepository.findByFreeUser();
-            if(coupon == null) {
-                coupon = save(member);
+            Optional<Coupon> coupon = couponRepository.findByFreeUser();
+            Coupon couponResult;
+            if(!coupon.isPresent()) {
+                couponResult = saveByMember(member);
             } else {
+                couponResult = coupon.get();
                 LocalDateTime assignedAt = LocalDateTime.now();
-                coupon.setMember(member);  // update SQL
-                coupon.setAssignedAt(assignedAt);
-                coupon.setExpiredAt(getRandomExpiredAt(assignedAt));
+                couponResult.setMember(member);  // update SQL
+                couponResult.setAssignedAt(assignedAt);
+                couponResult.setExpiredAt(getRandomExpiredAt(assignedAt));
             }
-            return coupon.getCode();
+            return couponResult.getCode();
         }).exceptionally((ex) -> {
-            ex.printStackTrace();
-            return null;
+            log.error(ex.getLocalizedMessage());
+            // TODO: change definite exception class.
+            throw new RuntimeException(ex.getLocalizedMessage());
         });
         log.info("current Thread name : " + Thread.currentThread().getName());
-        return future.get();
+        return future;
     }
 
     @CacheEvict(value="coupon-list", key="#member.id")
     @LogExecutionTime
     @Transactional
     public String assignToUser(Member member) throws InterruptedException {
-        // TODO: 트랜잭션 레벨 고려 (Coupon entity에 versioning 추가)
-        Coupon coupon = couponRepository.findByFreeUser();
-        if(coupon == null) {
-            coupon = save(member);
+        Optional<Coupon> coupon = couponRepository.findByFreeUser();
+        Coupon couponResult;
+        if(!coupon.isPresent()) {
+            couponResult = saveByMember(member);
         } else {
+            couponResult = coupon.get();
             LocalDateTime assignedAt = LocalDateTime.now();
-            coupon.setMember(member);  // update SQL
-            coupon.setAssignedAt(assignedAt);
-            coupon.setExpiredAt(getRandomExpiredAt(assignedAt));
+            couponResult.setMember(member);  // update SQL
+            couponResult.setAssignedAt(assignedAt);
+            couponResult.setExpiredAt(getRandomExpiredAt(assignedAt));
         }
-
-        log.info("current Thread name : " + Thread.currentThread().getName());
-        return coupon.getCode();
+        return couponResult.getCode();
     }
 
     public String testTransactionalProxy(Member m) throws InterruptedException {
