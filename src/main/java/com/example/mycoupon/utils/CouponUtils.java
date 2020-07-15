e25a4853a4a018c219eddd43751099957c7197a2
JisooJang <ppuagirls@naver.com>
2020-07-15 19:07:31 +0900

    feat: add CouponUtils class

---
+++ src/main/java/com/example/mycoupon/utils/CouponUtils.java
@@ -0,0 +1,16 @@
+package com.example.mycoupon.utils;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CouponUtils {
+    public static String getUUIDCouponCode() {
+        // format : uuid
+        return UUID.randomUUID().toString();
+    }
+
+    public static LocalDateTime getRandomExpiredAt(LocalDateTime fromDate) {
+        // add random expired days from now date (1 day ~ 7 days)
+        return fromDate.plusDays((long)(Math.random() * 7) + 1);
+    }
+}
